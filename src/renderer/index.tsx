13636293eb2814510b31f0f068b3460160cf873a
Kanerix <dkkasjoe@hotmail.com>
2022-01-14 14:02:23 +0100

    Fixed notify bug

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,12 +2,13 @@ import ReactDOM from "react-dom";
 import { Button } from "@mui/material";
 
 function App() {
+	const notify = () => {
+		// @ts-ignore
+		electron.notificationApi.notify("Tester med din mor");
+	};
+
 	return (
-		<Button
-			variant="contained"
-			// @ts-ignore
-			onClick={electron.notificationApi.notify("test")}
-		>
+		<Button variant="contained" onClick={notify}>
 			Hello World
 		</Button>
 	);
